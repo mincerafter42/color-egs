49fbe6ed1965208208fc4d6b0e601099a4c684b0
mincerafter42 <mincerafter42@gmail.com>
2020-04-11 10:51:45 -0700

    Added a simple HTML viewer
    Uses original EGS comics when they are already colored, by having a list of originally colored comics. Uses query string for comic index

---
+++ coloredOriginally.js
@@ -0,0 +1,12 @@
+//documents the comics that are already colored, therefore not stored in this repository.
+//has the comic's sequential number, and the URL of it on the egscomics.com server
+var alreadyColored={
+7:"20020127_pGFq.jpg",
+14:"20020203_hyJ6.png"/*,
+21:"20020210_BCBO.png",
+28:"20020217_nmtb.png",
+29:"20020218_Ezye.gif",
+35:"20020224_LVSJ.png",
+42:"20020303_lY9t.png",
+49:"20020310_i0Ia.png"*/
+}
